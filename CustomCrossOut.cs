3a7ae99a580eb79f39d8000296d92681beb68b65
DoggieLicc <doggie@doggieli.cc>
2025-07-11 22:56:53 -0400

    formatting

--- CustomCrossOut.cs
+++ CustomCrossOut.cs
@@ -7,35 +7,24 @@ using Game.Interface;
 using TMPro;
 using UnityEngine.UI;
 
-namespace CustomCrossOut
-{
-
+namespace CustomCrossOut {
     [SML.Mod.SalemMod]
-    public class CustomCrossOut
-    {
-
-        public void Start()
-        {
-            try
-            {
+    public class CustomCrossOut {
+        public void Start() {
+            try {
                 Harmony.CreateAndPatchAll(typeof(CustomCrossOut));
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 Console.WriteLine(e.Message);
             }
-
             Console.WriteLine("common foxfox W");
         }
     }
 
     [HarmonyPatch(typeof(RoleListItem), "ValidateCrossOut")]
-    class ValidateCrossOut_Patch 
-    {
-        public static bool Prefix(ref RoleListItem __instance)
-        {
-            if (__instance.isCrossedOut)
-            {
+    class ValidateCrossOut_Patch {
+        public static bool Prefix(ref RoleListItem __instance) {
+            if (__instance.isCrossedOut) {
                 int crossedOutOpacity = ModSettings.GetInt("Crossed Out Opacity %");
                 bool addStrikethrough = ModSettings.GetBool("Strikethrough Line");
 
