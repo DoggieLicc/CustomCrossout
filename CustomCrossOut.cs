aaf4c57189b9fe5671492cb5f4bb763db55b9887
DoggieLicc <doggie@doggieli.cc>
2025-07-11 22:31:18 -0400

    initial Commit

---
+++ CustomCrossOut.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using SML;
+using System;
+
+using Game.Interface;
+using TMPro;
+using UnityEngine.UI;
+
+namespace CustomCrossOut
+{
+
+    [SML.Mod.SalemMod]
+    public class CustomCrossOut
+    {
+
+        public void Start()
+        {
+            try
+            {
+                Harmony.CreateAndPatchAll(typeof(CustomCrossOut));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("common foxfox W");
+        }
+    }
+
+    [HarmonyPatch(typeof(RoleListItem), "ValidateCrossOut")]
+    class ValidateCrossOut_Patch 
+    {
+        public static bool Prefix(ref RoleListItem __instance)
+        {
+            if (__instance.isCrossedOut)
+			{
+                int crossedOutOpacity = ModSettings.GetInt("Crossed Out Opacity %");
+                bool addStrikethrough = ModSettings.GetBool("Strikethrough Line");
+
+                if (addStrikethrough) {
+				    __instance.roleLabel.GetComponent<TMP_Text>().fontStyle |= FontStyles.Strikethrough;
+                }
+
+				__instance.roleLabel.GetComponent<TMP_Text>().alpha = crossedOutOpacity / 100f;
+			}
+            else {
+                __instance.roleLabel.GetComponent<TMP_Text>().fontStyle &= ~FontStyles.Strikethrough;
+                __instance.roleLabel.GetComponent<TMP_Text>().alpha = 1.0f;
+            }
+            return false;
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+﻿# CustomCrossOut
+Lets you customize how crossed out roles appear in the rolelist.
+
+- Change opacity of crossed out roles
+- Disable the strikethrough on crossed out roles
+
+### Example with changed opacity
+![Example](https://doggieli.cc/assets/images/customcrossout.png)
