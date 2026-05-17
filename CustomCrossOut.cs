0d582a88885c2ee9a570be3b72201415782a4b53
DoggieLicc <doggie@doggieli.cc>
2026-05-17 13:12:34 -0600

    Remove itallics for compatability reasons

--- CustomCrossOut.cs
+++ CustomCrossOut.cs
@@ -1,20 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using Game.Interface;
 using HarmonyLib;
 using SML;
-using System;
-
-using Game.Interface;
 using TMPro;
-using UnityEngine.UI;
 
-namespace CustomCrossOut {
+namespace CustomCrossOut
+{
     [SML.Mod.SalemMod]
-    public class CustomCrossOut {
-        public void Start() {
-            try {
+    public class CustomCrossOut
+    {
+        public void Start()
+        {
+            try
+            {
                 Harmony.CreateAndPatchAll(typeof(CustomCrossOut));
             }
-            catch (Exception e) {
+            catch (Exception e)
+            {
                 Console.WriteLine(e.Message);
             }
             Console.WriteLine("common foxfox W");
@@ -22,31 +24,30 @@ namespace CustomCrossOut {
     }
 
     [HarmonyPatch(typeof(RoleListItem), "ValidateCrossOut")]
-    class ValidateCrossOut_Patch {
-        public static bool Prefix(ref RoleListItem __instance) {
+    class ValidateCrossOut_Patch
+    {
+        public static bool Prefix(ref RoleListItem __instance)
+        {
             TMP_Text roleLabelText = __instance.roleLabel.GetComponent<TMP_Text>();
 
-            if (__instance.isCrossedOut) {
+            if (__instance.isCrossedOut)
+            {
                 int crossedOutOpacity = ModSettings.GetInt("Crossed Out Roles Opacity %");
                 bool addStrikethrough = ModSettings.GetBool("Strikethrough Crossed Out Roles");
-                bool addItalics = ModSettings.GetBool("Italicize Crossed Out Roles");
                 int fontSize = ModSettings.GetInt("Crossed Out Roles size");
 
-                if (addStrikethrough) {
+                if (addStrikethrough)
+                {
                     roleLabelText.fontStyle |= FontStyles.Strikethrough;
-                }
-
-                if (addItalics) {
-                    roleLabelText.fontStyle |= FontStyles.Italic;
                 }
 
                 roleLabelText.alpha = crossedOutOpacity / 100f;
                 roleLabelText.fontSizeMin = fontSize;
                 roleLabelText.fontSizeMax = fontSize;
             }
-            else {
+            else
+            {
                 roleLabelText.fontStyle &= ~FontStyles.Strikethrough;
-                roleLabelText.fontStyle &= ~FontStyles.Italic;
                 roleLabelText.alpha = 1.0f;
                 roleLabelText.fontSizeMin = 25;
                 roleLabelText.fontSizeMax = 25;
@@ -54,5 +55,4 @@ namespace CustomCrossOut {
             return false;
         }
     }
-
 }
